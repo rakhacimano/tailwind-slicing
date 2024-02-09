4857799eeaddcb91f94472f6906798b193e295d2
Rakha Putra Pratama <rakhacimano@gmail.com>
2024-02-09 16:01:12 +0700

    style: done responsive brand section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
-  plugins: [],
 };
